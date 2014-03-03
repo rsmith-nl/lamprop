a7eac69e8e7e391ac4e588538922f205ed98aff3
Roland Smith <rsmith@xs4all.nl>
2014-03-03 21:44:48 +0100

    Used main function in lamprop.

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,10 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
 # lamprop - main program.
+# vim:fileencoding=utf-8
 # $Date$
+
+"""Calculate the elastic properties of a fibrous composite laminate.
+See lamprop(1) for the manual of this program and lamprop(5) for the manual
+of the data file format."""
 
 from __future__ import print_function, division
 
@@ -44,55 +47,55 @@ class LicenseAction(argparse.Action):
         print(_lic)
         sys.exit()
 
-desc = ('Calculate the elastic properties of a fibrous composite laminate.'
-        ' See lamprop(1) for the manual of this program and lamprop(5) '
-        'for the manual of the data file format.')
 
-# Process the command-line arguments
-opts = argparse.ArgumentParser(prog='lamprop', description=desc)
-group = opts.add_mutually_exclusive_group()
-group.add_argument('-l', '--latex', action='store_true', help="LaTeX output")
-group.add_argument('-H', '--html', action='store_true', help="HTML output")
-opts.add_argument('-e', '--eng', action='store_true',
-                  help="produce only the layers and engineering properties")
-opts.add_argument('-m', '--mat', action='store_true',
-                  help="produce only the ABD and abd matrices")
-group = opts.add_mutually_exclusive_group()
-group.add_argument('-L', '--license', action=LicenseAction, nargs=0,
-                   help="print the license")
-group.add_argument('-v', '--version', action='version',
-                   version=__version__)
-opts.add_argument("files", metavar='file', nargs='*',
-                  help="one or more files to process")
-args = opts.parse_args()
-del opts, group
-if args.mat is False and args.eng is False:
-    args.eng = True
-    args.mat = True
-elif args.mat and args.eng:
-    pass
-elif args.eng:
-    args.mat = False
-else:
-    args.eng = False
+def main(argv):
+    # Process the command-line arguments
+    opts = argparse.ArgumentParser(prog='lamprop', description=__doc__)
+    group = opts.add_mutually_exclusive_group()
+    group.add_argument('-l', '--latex', action='store_true',
+                       help="LaTeX output")
+    group.add_argument('-H', '--html', action='store_true', help="HTML output")
+    s = "produce only the layers and engineering properties"
+    opts.add_argument('-e', '--eng', action='store_true', help=s)
+    opts.add_argument('-m', '--mat', action='store_true',
+                      help="produce only the ABD and abd matrices")
+    group = opts.add_mutually_exclusive_group()
+    group.add_argument('-L', '--license', action=LicenseAction, nargs=0,
+                       help="print the license")
+    group.add_argument('-v', '--version', action='version',
+                       version=__version__)
+    opts.add_argument("files", metavar='file', nargs='*',
+                      help="one or more files to process")
+    args = opts.parse_args(argv)
+    del opts, group
+    if args.mat is False and args.eng is False:
+        args.eng = True
+        args.mat = True
+    elif args.mat and args.eng:
+        pass
+    elif args.eng:
+        args.mat = False
+    else:
+        args.eng = False
+    # No files given to process.
+    if len(args.files) == 0:
+        sys.exit(1)
+    # Set the output method.
+    out = text.out
+    if args.latex:
+        out = latex.out
+    elif args.html:
+        out = html.out
+    for f in args.files:
+        # Process the files.
+        laminates, messages = parse(f)
+        for m in messages:
+            if m.startswith('Error'):
+                print('\033[31m', m, '\033[0m', file=sys.stderr, sep='')
+            else:
+                print('\033[33m', m, '\033[0m', file=sys.stderr, sep='')
+        for curlam in laminates:
+            out(curlam, args.eng, args.mat)
 
-# No files given to process.
-if len(args.files) == 0:
-    sys.exit(1)
-
-# Set the output method.
-out = text.out
-if args.latex:
-    out = latex.out
-elif args.html:
-    out = html.out
-for f in args.files:
-    # Process the files.
-    laminates, messages = parse(f)
-    for m in messages:
-        if m.startswith('Error'):
-            print('\033[31m', m, '\033[0m', file=sys.stderr, sep='')
-        else:
-            print('\033[33m', m, '\033[0m', file=sys.stderr, sep='')
-    for curlam in laminates:
-        out(curlam, args.eng, args.mat)
+if __name__ == '__main__':
+    main(sys.argv[1:])
