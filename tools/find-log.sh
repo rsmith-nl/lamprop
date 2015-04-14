5014334792b514e68ab64a2995e98c8be6326dbc
Roland Smith <rsmith@xs4all.nl>
2015-04-14 14:18:48 +0200

    Added tool to find logging in source code.

---
+++ tools/find-log.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# file: find-log.sh
+# vim:fileencoding=utf-8:ft=sh
+#
+# Find all the lines in the source code where the Logging functions or Logger
+# methods error, warning, debug or info are used.
+#
+# Author: R.F. Smith <rsmith@xs4all.nl>
+# Created: 2015-04-14 14:15:00 +0200
+# $Date$
+# $Revision$
+
+find src -type f -name '*.py' -exec egrep -H -B 1 '\.(error|warning|debug|info)' {} \;
