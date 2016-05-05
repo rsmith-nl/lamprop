2ac68a9f4488f633d01c26751f006b8b5d4f4fb0
Roland Smith <rsmith@xs4all.nl>
2016-05-05 13:42:07 +0200

    Update build script.

--- build.py
+++ build.py
@@ -1,41 +1,60 @@
-#!/usr/bin/env python3
 # file: build.py
 # vim:fileencoding=utf-8:ft=python
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
-# Created: 2016-03-31 11:59:23 +0200
-# Last modified: 2016-04-03 12:16:32 +0200
+# Created: 2016-04-24 17:06:48 +0200
+# Last modified: 2016-05-05 13:41:25 +0200
 
-"""Build an archive executable for the lamprop program."""
+"""Create runnable archives from program files and custom modules."""
 
 import os
-import io
+import py_compile
+import tempfile
 import zipfile as z
 
 
-def buildarchive(name, srcdir, modules):
-    """
-    Build an executable archive for a program.
+def mkarchive(name, modules, main='__main__.py',
+              shebang=b'#!/usr/bin/env python3\n'):
+    """Create a runnable archive.
 
     Arguments:
-        name: Name of the program without extension.
-        srcdir: Directory to archive. This should contain __main__.py
-        modules: module name or list of module names in srcdir.
+        name: Name of the archive.
+        modules: Module name or iterable of module names to include.
+        main: Name of the main file. Defaults to __main__.py
+        shebang: Description of the interpreter to use. Defaults to Python 3.
     """
+    std = '__main__.py'
     if isinstance(modules, str):
         modules = [modules]
-    origdir = os.getcwd()
-    os.chdir(srcdir)
-    tmpf = io.BytesIO()
+    if main != std:
+        try:
+            os.remove(std)
+        except OSError:
+            pass
+        os.link(main, std)
+    # Forcibly compile __main__.py lest we use an old version!
+    py_compile.compile(std)
+    tmpf = tempfile.TemporaryFile()
     with z.PyZipFile(tmpf, mode='w', compression=z.ZIP_DEFLATED) as zf:
-        zf.writepy('__main__.py')
+        zf.writepy(std)
         for m in modules:
             zf.writepy(m)
-    os.chdir(origdir)
+    if main != std:
+        os.remove(std)
+    tmpf.seek(0)
+    archive_data = tmpf.read()
+    tmpf.close()
     with open(name, 'wb') as archive:
-        archive.write(b'#!/usr/bin/env python3\n')
-        archive.write(tmpf.getvalue())
+        archive.write(shebang)
+        archive.write(archive_data)
     os.chmod(name, 0o755)
 
 
-buildarchive('lamprop', 'src', 'lamprop')
+if __name__ == '__main__':
+    from shutil import copy
+    archname = 'lpbin'
+    name = 'lamprop'
+    os.chdir('src')
+    mkarchive(archname, name)
+    copy(archname, '../'+name)
+    os.remove(archname)
