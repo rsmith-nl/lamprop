fd1c664c3b939102b1e683f2d80859d5492443f1
Roland Smith <rsmith@xs4all.nl>
2016-06-05 21:50:47 +0200

    Update tools/find-log to only search for warnings and errors.

--- tools/find-log.sh
+++ tools/find-log.sh
@@ -3,11 +3,10 @@
 # vim:fileencoding=utf-8:ft=sh
 #
 # Find all the lines in the source code where the Logging functions or Logger
-# methods error, warning, debug or info are used.
+# methods error or warning are used.
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2015-04-14 14:15:00 +0200
-# $Date$
-# $Revision$
+# Last modified: 2016-06-05 21:29:06 +0200
 
-find src -type f -name '*.py' -exec egrep -H -B 1 '\.(error|warning|debug|info)' {} \;
+find src -type f -name '*.py' -exec egrep -H -B 1 '\.(error|warning)\(' {} \;
