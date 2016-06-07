7ad1602450b3382cdba02d9e144c7b1966f51fc7
Roland Smith <rsmith@xs4all.nl>
2016-06-07 20:55:37 +0200

    Improvements to lam2json.
    - Use ordered dictionaries.
    - Join JSON dicts on a line by default.
    - Add --stdout and --nojoin options.
    - Better error handling in parsing.

--- tools/lam2json.py
+++ tools/lam2json.py
@@ -4,19 +4,20 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2016-06-06 22:21:33 +0200
-# Last modified: 2016-06-07 00:17:42 +0200
+# Last modified: 2016-06-07 20:43:36 +0200
 
 """Script to convert a lamprop version 2 `lam` file to version 3 JSON
 format."""
 
+from collections import OrderedDict as Od
 import argparse
 import json
+import logging
 import os
 import re
 import sys
-import logging
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 
 def main(argv):
@@ -35,8 +36,15 @@ def main(argv):
         newpath = re.sub(sre, r'\1.json', path)
         logging.info('Converting {} to {}.'.format(path, newpath))
         data = process_file(path)
-        with open(newpath, 'w') as outfile:
-            outfile.write(data)
+        if not args.nojoin:
+            # Reformat the data
+            data = re.sub(r'([^\}\]]),\s*\n\s*', r'\1, ', data,
+                          flags=re.MULTILINE)
+        if args.stdout:
+            print(data)
+        else:
+            with open(newpath, 'w') as outfile:
+                outfile.write(data)
 
 
 def process_arguments(argv):
@@ -44,9 +52,13 @@ def process_arguments(argv):
     opts = argparse.ArgumentParser(prog='lam2json', description=__doc__)
     opts.add_argument('-v', '--version', action='version',
                       version=__version__)
+    opts.add_argument('-s', '--stdout', action='store_true',
+                      help='output to standard output (default: to file)')
+    opts.add_argument('-j', '--nojoin', action='store_true',
+                      help='print each value on a single line (default: join)')
     opts.add_argument('--log', default='warning',
                       choices=['debug', 'info', 'warning', 'error'],
-                      help="logging level (defaults to 'warning')")
+                      help='logging level (defaults to "warning")')
     opts.add_argument("files", metavar='file', nargs='*',
                       help="one or more files to process")
     args = opts.parse_args(argv)
@@ -57,7 +69,8 @@ def process_arguments(argv):
 
 def process_file(path):
     T, M, L = 1, 2, 3
-    out = {}
+    es = 'invalid line "{}" skipped'
+    out = Od()
     with open(path) as infile:
         lines = infile.readlines()
     # Only directives
@@ -66,52 +79,71 @@ def process_file(path):
     fl = [ln for ln in lines if ln[0] is 'f']
     out['fibers'] = []
     for ln in fl:
-        _, E1, nu12, alpha1, density, name = ln.split(maxsplit=5)
-        out['fibers'].append({"E1": float(E1), "nu12": float(nu12),
-                              "alpha1": float(alpha1),
-                              "density": float(density), "name": name})
+        try:
+            _, E1, nu12, alpha1, density, name = ln.split(maxsplit=5)
+            newf = Od()
+            newf["E1"] = float(E1)
+            newf["nu12"] = float(nu12)
+            newf["alpha1"] = float(alpha1)
+            newf["density"] = float(density)
+            newf["name"] = name
+            out['fibers'].append(newf)
+        except ValueError:
+            logging.error(es.format(ln))
     rl = [ln for ln in lines if ln[0] is 'r']
     out['resins'] = []
     for ln in rl:
-        _, E, nu, alpha, density, name = ln.split(maxsplit=5)
-        out['resins'].append({"E": float(E1), "nu": float(nu), "alpha":
-                              float(alpha), "density": float(density),
-                              "name": name})
+        try:
+            _, E, nu, alpha, density, name = ln.split(maxsplit=5)
+            newr = Od()
+            newr["E"] = float(E1)
+            newr["nu"] = float(nu)
+            newr["alpha"] = float(alpha)
+            newr["density"] = float(density)
+            newr["name"] = name
+            out['resins'].append(newr)
+        except ValueError:
+            logging.error(es.format(ln))
     state = T
     out['laminates'] = []
-    laminate = {}
+    laminate = Od()
     for ln in lines:
-        if state == L and ln[0] == 't':
-            out['laminates'].append(laminate)
-            state = T
-            # no continue!
-        if state == T and ln[0] == 't':
-            laminate = {}
-            laminate['name'] = ln.split(maxsplit=1)[1]
-            state = M
-            continue
-        if state == M and ln[0] == 'm':
-            _, vf, rname = ln.split(maxsplit=2)
-            laminate['vf'] = float(vf)
-            laminate['matrix'] = rname
-            laminate['lamina'] = []
-            state = L
-            continue
-        if state == L and ln[0] == 'l':
-            _, weight, angle, *rest = ln.split(maxsplit=4)
-            lamina = {'weight': float(weight), 'angle': float(angle)}
-            try:
-                lamina['vf'] = float(rest[0])
-                del rest[0]
-            except ValueError:
-                pass
-            lamina['fiber'] = ' '.join(rest)
-            laminate['lamina'].append(lamina)
-            continue
-        if state == L and ln[0] == 's':
-            laminate['symmetric'] = True
-            out['laminates'].append(laminate)
-            state = T
+        try:
+            if state == L and ln[0] == 't':
+                out['laminates'].append(laminate)
+                state = T
+                # no continue!
+            if state == T and ln[0] == 't':
+                laminate = Od()
+                laminate['name'] = ln.split(maxsplit=1)[1]
+                state = M
+                continue
+            if state == M and ln[0] == 'm':
+                _, vf, rname = ln.split(maxsplit=2)
+                laminate['vf'] = float(vf)
+                laminate['matrix'] = rname
+                laminate['lamina'] = []
+                state = L
+                continue
+            if state == L and ln[0] == 'l':
+                _, weight, angle, *rest = ln.split(maxsplit=4)
+                lamina = Od()
+                lamina['weight'] = float(weight)
+                lamina['angle'] = float(angle)
+                try:
+                    lamina['vf'] = float(rest[0])
+                    del rest[0]
+                except ValueError:
+                    pass
+                lamina['fiber'] = ' '.join(rest)
+                laminate['lamina'].append(lamina)
+                continue
+            if state == L and ln[0] == 's':
+                laminate['symmetric'] = True
+                out['laminates'].append(laminate)
+                state = T
+        except ValueError:
+            logging.error(es.format(ln))
     if 'lamina' in laminate:
         out['laminates'].append(laminate)
     return json.dumps(out, indent=2)
