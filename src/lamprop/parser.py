c95a7cce31a36b0f717333d16aae69a23c5248c7
Roland Smith <rsmith@xs4all.nl>
2016-06-07 22:22:15 +0200

    Refactor parser. Version 2.3.0.

--- src/lamprop/parser.py
+++ src/lamprop/parser.py
@@ -2,7 +2,7 @@
 # vim:fileencoding=utf-8:ft=python
 # Copyright © 2014-2016 R.F. Smith <rsmith@xs4all.nl>. All rights reserved.
 # Created: 2014-02-21 21:35:41 +0100
-# Last modified: 2016-05-29 13:24:33 +0200
+# Last modified: 2016-06-07 22:19:24 +0200
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -30,6 +30,8 @@
 import logging
 from .types import Fiber, Resin, lamina, laminate
 
+msg = logging.getLogger('parser')
+
 
 def parse(filename):
     """Parses a lamprop file.
@@ -40,7 +42,6 @@ def parse(filename):
     Returns:
         A list of laminates.
     """
-    msg = logging.getLogger('parser')
     try:
         with open(filename, encoding='utf-8') as df:
             data = [ln.strip() for ln in df]
@@ -51,84 +52,59 @@ def parse(filename):
     directives = [(num, ln) for num, ln in enumerate(data, start=1)
                   if len(ln) > 1 and ln[1] is ':' and ln[0] in 'tmlsfr']
     msg.info("found {} directives in '{}'".format(len(directives), filename))
-    fibers = [_f(ln, num, msg) for num, ln in directives if ln[0] is 'f']
-    _rmdup(fibers, 'fibers', msg)
+    fibers = [_f(ln, num) for num, ln in directives if ln[0] is 'f']
+    _rmdup(fibers, 'fibers')
     msg.info("found {} fibers in '{}'".format(len(fibers), filename))
     fdict = {fiber.name: fiber for fiber in fibers}
-    resins = [_r(ln, num, msg) for num, ln in directives if ln[0] is 'r']
-    _rmdup(resins, 'resins', msg)
+    resins = [_r(ln, num) for num, ln in directives if ln[0] is 'r']
+    _rmdup(resins, 'resins')
     msg.info("found {} resins in '{}'".format(len(resins), filename))
     rdict = {resin.name: resin for resin in resins}
     directives = [(num, ln) for num, ln in directives if ln[0] in 'tmls']
-    tindices = [i for i, (_, ln) in enumerate(directives) if ln[0] is 't']
-    if not tindices:
-        msg.error("no laminates found in '{}'".format(filename))
-        return []
-    else:
-        msg.info("found {} laminates in '{}'".format(len(tindices), filename))
-    tindices.append(len(directives))
-    pairs = zip(tindices, tindices[1:])
-    laminatedata = [directives[start:stop] for start, stop in pairs]
+    T, M, L = 1, 2, 3
+    state = T
+    name, vf, matrix, llist = None, None, None, []
     laminates = []
-    lamnames = []
-    for lam in laminatedata:
-        symmetric = False
-        numt, t = lam[0]
-        numm, m = lam[1]
-        name = t[2:].strip()
-        if name in lamnames:
-            s = "laminate '{}' on line {} already exists. Skipping laminate."
-            msg.warning(s.format(name, numt))
-            continue
-        lamnames.append(name)
-        if m[0] is not 'm':
-            s = "no 'm:' on line {}. Skipping laminate."
-            msg.warning(s.format(numt+1))
-            continue
-        items = m.split(None, 2)
+    for num, ln in directives:
         try:
-            vf = float(items[1])
-            mname = items[2]
-        except ValueError:
-            vf = None
-            mname = items[1]
-        try:
-            resin = rdict[mname]
-        except KeyError:
-            s = "unknown resin '{}' on line {}. Skipping laminate."
-            msg.error(s.format(mname, numm))
-            continue
-        layers = []
-        if lam[-1][1].startswith('s'):
-            symmetric = True
-            msg.info("found symmetry directive in '{}'.".format(name))
-            del lam[-1]
-        try:
-            for numl, l in lam[2:]:
-                if l[0] is not 'l':
-                    s = "unexpected '{}:' on line {}. Skipping laminate."
-                    raise ValueError(s.format(l[0], numl))
-                else:
-                    values = _l(l, numl, resin, vf, msg)
+            if state == L and ln[0] == 't':
+                laminates.append(laminate(name, llist))
+                state = T
+                # no continue!
+            if state == T and ln[0] == 't':
+                vf, matrix = None, None
+                name = ln.split(maxsplit=1)[1]
+                state = M
+                continue
+            if state == M and ln[0] == 'm':
+                _, vf, rname = ln.split(maxsplit=2)
+                vf = float(vf)
+                matrix = rdict[rname]
+                llist = []
+                state = L
+                continue
+            if state == L and ln[0] == 'l':
+                _, weight, angle, *rest = ln.split(maxsplit=4)
+                weight, angle = float(weight), float(angle)
                 try:
-                    fiber = fdict[values[0]]
-                    values[0] = fiber
-                except KeyError:
-                    s = "unknown fiber '{}' on line {}. Skipping laminate."
-                    raise ValueError(s.format(values[0], numl))
-                    continue
-                layers.append(lamina(*values))
-        except ValueError as e:
-            msg.error(e)
-            continue
-        if not layers:
-            msg.warning("empty laminate '{}'. Skipping".format(name))
-            continue
-        if symmetric:
-            extra = layers.copy()
-            extra.reverse()
-            layers += extra
-        laminates.append(laminate(name, layers))
+                    vf = float(rest[0])
+                    del rest[0]
+                except ValueError:
+                    pass
+                fibername = ' '.join(rest)
+                la = lamina(fdict[fibername], matrix, weight, angle, vf)
+                llist.append(la)
+                continue
+            if state == L and ln[0] == 's':
+                llist = llist + list(reversed(llist))
+                laminates.append(laminate(name, llist))
+                state = T
+        except ValueError:
+            logging.error('invalid line {} "{}" skipped'.format(num, ln))
+        except KeyError:
+            logging.error('line {} unknown "{}"'.format(num, ln))
+    if len(llist):
+        laminates.append(laminate(name, llist))
     return laminates
 
 
@@ -148,7 +124,7 @@ def _num(val):
     return True
 
 
-def _f(line, number, log):
+def _f(line, number):
     """Parse a line describing a fiber.
 
     f: <E1> <Poisson's ratio 1,2> <CTE1> <density> <name>
@@ -163,7 +139,7 @@ def _f(line, number, log):
     test = line.split()
     try:
         if _num(test[5]):  # old format
-            log.info("old style fiber on line {}".format(number))
+            msg.info("old style fiber on line {}".format(number))
             items = line.split(None, 8)
             indices = [1, 3, 5, 7, 8]
         else:
@@ -173,12 +149,12 @@ def _f(line, number, log):
         values.append(items[indices[4]])
         values.append(number)
     except (ValueError, IndexError) as e:
-        log.error('parsing a fiber on line {}; {}.'.format(number, e))
+        msg.error('parsing a fiber on line {}; {}.'.format(number, e))
         return None
     return Fiber(*values)
 
 
-def _r(line, number, log):
+def _r(line, number):
     """Parse a line describing a resin.
 
     r: <Ematrix> <Poisson's ratio> <CTE> <density> <name>
@@ -197,12 +173,12 @@ def _r(line, number, log):
         values.append(items[5])
         values.append(number)
     except ValueError as e:
-        log.error('parsing a resin on line {}; {}.'.format(number, e))
+        msg.error('parsing a resin on line {}; {}.'.format(number, e))
         return None
     return Resin(*values)
 
 
-def _l(line, number, resin, vf, log):
+def _l(line, number, resin, vf):
     """Parse a lamina line;
 
     l: <weight> <angle> [<vf>] <fiber name>
@@ -224,7 +200,7 @@ def _l(line, number, resin, vf, log):
         if vf is None:
             s = 'no vf in laminate line {}, and no global vf.'
             s = s.format(line)
-            log.error(s)
+            msg.error(s)
             raise ValueError(s)
         items = line.split(None, 3)
     values = [items[-1], resin]
@@ -233,7 +209,7 @@ def _l(line, number, resin, vf, log):
     return values
 
 
-def _rmdup(lst, name, log):
+def _rmdup(lst, name):
     """Remove duplicate or empty Fibers or Resins from the supplied list.
 
     Arguments:
@@ -249,5 +225,5 @@ def _rmdup(lst, name, log):
             names.append(i.name)
         else:
             s = "{} '{}' at line {} is a duplicate, will be ignored."
-            log.warning(s.format(name, i.name, i.line))
+            mag.warning(s.format(name, i.name, i.line))
             del(lst[n])
