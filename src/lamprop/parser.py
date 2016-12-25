45933584351ff05a971d972583def49f2724ba73
Roland Smith <rsmith@xs4all.nl>
2016-12-26 00:30:35 +0100

    More parser work.

--- src/lamprop/parser.py
+++ src/lamprop/parser.py
@@ -2,7 +2,7 @@
 # vim:fileencoding=utf-8:ft=python
 # Copyright © 2014-2016 R.F. Smith <rsmith@xs4all.nl>. All rights reserved.
 # Created: 2014-02-21 21:35:41 +0100
-# Last modified: 2016-12-03 15:33:26 +0100
+# Last modified: 2016-12-13 23:09:22 +0100
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -34,7 +34,9 @@ from .types import Fiber, Resin, Lamina, Laminate
 msg = logging.getLogger('parser')
 
 _numre = r'([+-]?\d+(?:\.\d*)?(?:[eE][+-]?\d+)?)'
+_numspcre = r'(?:([+-]?\d+(?:\.\d*)?(?:[eE][+-]?\d+)?\s+))'
 _spcre = r'\s+'
+_namere = r'(.*?)\s*$'
 
 
 def parse(filename):
@@ -56,69 +58,24 @@ def parse(filename):
     directives = [(num, ln) for num, ln in enumerate(data, start=1)
                   if len(ln) > 1 and ln[1] is ':' and ln[0] in 'tmlsfr']
     msg.info("found {} directives in '{}'".format(len(directives), filename))
-    fibers = find_components(directives, 'f:')
+    fibers = _components(directives, 'f:')
     msg.info("found {} fibers in '{}'".format(len(fibers), filename))
     fdict = {fiber.name: fiber for fiber in fibers}
-    resins = find_components(directives, 'r:')
+    resins = _components(directives, 'r:')
     msg.info("found {} resins in '{}'".format(len(resins), filename))
     rdict = {resin.name: resin for resin in resins}
-    directives = [(num, ln) for num, ln in directives if ln[0] in 'tmls']
-    T, M, L = 1, 2, 3
-    state = T
-    name, vf, matrix, llist = None, None, None, []
-    laminates = []
-    for num, ln in directives:
-        try:
-            if state == L and ln[0] == 't':
-                laminates.append(Laminate(name, llist))
-                state = T
-                llist = None
-                # no continue!
-            if state == T and ln[0] == 't':
-                vf, matrix = None, None
-                name = ln.split(maxsplit=1)[1]
-                state = M
-                continue
-            if state == M and ln[0] == 'm':
-                _, vf, rname = ln.split(maxsplit=2)
-                vf = float(vf)
-                matrix = rdict[rname]
-                llist = []
-                state = L
-                continue
-            if state == L and ln[0] == 'l':
-                _, weight, angle, *rest = ln.split(maxsplit=4)
-                weight, angle = float(weight), float(angle)
-                try:
-                    vf = float(rest[0])
-                    del rest[0]
-                except ValueError:
-                    pass
-                fibername = ' '.join(rest)
-                la = Lamina(fdict[fibername], matrix, weight, angle, vf)
-                llist.append(la)
-                continue
-            if state == L and ln[0] == 's':
-                llist = llist + list(reversed(llist))
-                laminates.append(Laminate(name, llist))
-                state = T
-                llist = None
-        except ValueError:
-            logging.error('invalid line {} "{}" skipped'.format(num, ln))
-        except KeyError:
-            logging.error('line {} unknown "{}"'.format(num, ln))
-    if llist:
-        laminates.append(Laminate(name, llist))
+    laminates = _laminates(directives, rdict, fdict)
     return laminates
 
 
-def find_components(directives, what):
+def _components(directives, what):
     """Finds fibers or resins.
 
     Arguments:
-        directives: A sequence of strings containing directives.
-            A directive is a line whose first non-space character is one of
-            'f', 'r', 't', 'm', 'l' or 's', and whose second character is ':'.
+        directives: A sequence of 2-tuples (int, string) containing a line
+            number and a directive. A directive is a line whose first non-space
+            character is one of 'f', 'r', 't', 'm', 'l' or 's', and whose
+            second character is ':'.
         what: The key of the component to look for. 'f:' for fibers, 'r:' for
             resins.
     Returns:
@@ -127,49 +84,90 @@ def find_components(directives, what):
     choice = {'f:': Fiber, 'r:': Resin}
     rv = []
     names = []
-    cre = _spcre.join([what, _numre, _numre, _numre, _numre, '(.*)$'])
-    for num, ln in directives:
+    cre = ''.join([what, _spcre, _numspcre + r'{4}', _namere])
+    for num, ln in (d for d in directives if d[1].startswith(what)):
+        name = re.findall(_namere, ln)[0]
+        numbers = re.findall(_numre, ln)[:4]
+
         match = re.fullmatch(cre, ln)
         if match:
+            print('groups:', match.groups())
             try:
                 found = choice[what](*match.groups())
                 if found.name in names:
                     s = "line {} is a duplicate {} '{}', will be ignored."
-                    logging.warning(s.format(num, what, found.name))
+                    msg.warning(s.format(num, what, found.name))
                 else:
                     rv.append(found)
-            except ValueError as e:
+            except (ValueError, TypeError) as e:
                 s = 'line {} is not a valid {}: {}'
-                logging.error(s.format(num, type(what).__name__, e))
+                msg.error(s.format(num, type(choice[what]).__name__, e))
     return rv
 
 
-def _l(line, number, resin, vf):
-    """Parse a lamina line;
+def _laminates(directives, resin_dict, fiber_dict):
+    laminate_directives = [(num, ln) for (num, ln) in directives if ln[0]
+                           in 'tmls']
+    laminate_keystring = ''.join(d[1][0] for d in laminate_directives)
+    laminates = []
+    for match in re.finditer('tml+s?', laminate_keystring):
+        layers = []
+        start, end = match.start(), match.end()
+        try:
+            laminate_name, global_vf, resin = \
+                _parameters(laminate_directives, start, resin_dict)
+        except ValueError as e:
+            msg.warning(e)
+            continue
+        lastline = laminate_directives[end-1][1]
+        if lastline.startswith('s'):
+            end -= 1
+        for k in range(start+2, end):
+            try:
+                layers.append(_lamina(laminate_directives[k][0],
+                                      laminate_directives[k][1],
+                                      global_vf, fiber_dict, resin))
+            except ValueError as e:
+                msg.warning(e)
+        if lastline.startswith('s'):
+            layers + list(reversed(layers))
+    else:  # complete laminate
+        if layers:
+            laminates.append(Laminate(laminate_name, layers))
+    return laminates
 
-    l: <weight> <angle> [<vf>] <fiber name>
 
-    Arguments:
-        line: String to parse.
-        number: Line number in the original file.
-        resin: The resin type to use.
-        vf: Global fiber volume fraction.
-        log: A Logger instance to report failures.
-
-    Returns:
-        A tuple to initialize a Lamina.
-    """
-    items = line.split(None, 4)
+def _parameters(directives, start, resin_dict):
+    s = 'illegal "{}" directive on line {}'
+    # tre = r't:' + _spcre + _namere
+    mre = _spcre.join([r'm:', _numre, _namere])
     try:
-        vf = float(items[3])
-    except ValueError:
-        if vf is None:
-            s = 'no vf in laminate line {}, and no global vf.'
-            s = s.format(line)
-            msg.error(s)
-            raise ValueError(s)
-        items = line.split(None, 3)
-    values = [items[-1], resin]
-    values += [float(j) for j in items[1:3]]
-    values.append(vf)
-    return values
+        laminate_name = directives[start][1].split(maxsplit=1)[1]
+    except IndexError:
+        raise ValueError(s.format('m:', directives[start][0]))
+    try:
+        global_vf, resin_name = \
+            re.findall(mre, directives[start+1][1])[0]
+    except (ValueError, IndexError):
+        raise ValueError(s.format('t:', directives[start][0]))
+    if resin_name not in resin_dict.keys():
+        s = 'skipping laminate with unknown resin "{}" on line {}'
+        raise ValueError(s.format(resin_name, directives[start][0]))
+    return laminate_name, global_vf, resin_dict[resin_name]
+
+
+def _lamina(num, line, vf, fiber_dict, resin):
+    numbers = re.findall(_numre, line)
+    if len(numbers) == 2:
+        numbers = (numbers[0], numbers[1], vf)
+    elif len(numbers) == 3:
+        pass
+    else:
+        s = 'illegal "{}" directive on line {}'
+        msg.warning(s.format('l:', num))
+    fiber_name = re.findall(_namere, line)
+    if fiber_name not in fiber_dict.keys():
+        s = 'skipping lamina with unknown fiber "{}" on line {}'
+        raise ValueError(s.format(fiber_name, line))
+    numbers = [float(f) for f in numbers]
+    return Lamina(fiber_dict[fiber_name], resin, *numbers)
