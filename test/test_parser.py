b082ea3b53f12f9f1b1144989764724ada217a5f
Roland Smith <rsmith@xs4all.nl>
2017-02-25 15:18:32 +0100

    Update tests.

--- test/test_parser.py
+++ test/test_parser.py
@@ -3,7 +3,7 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2016-06-08 22:10:46 +0200
-# Last modified: 2017-02-25 11:06:28 +0100
+# Last modified: 2017-02-25 16:36:48 +0100
 
 
 """Test for lamprop parser."""
@@ -12,7 +12,31 @@ import sys
 
 sys.path.insert(1, 'src')
 
-from lamprop.parser import _get_components, Resin, Fiber, _l  # noqa
+from lamprop.parser import _get_numbers, _get_components, _get_lamina, Fiber, Resin  # noqa
+
+
+def test_numbers():
+    directives = [(2, 'f:  238000  0.25    -0.1e-6     1.76    TenaxHTA'),
+                  (3, 'f:  240000  0.25    -0.1e-6     1.77    TenaxHTS'),
+                  (4, 'f:  240000  0.25    -0.12e-6    1.78    Tenax STS40'),
+                  (5, 'f:  230000  0.27    -0.41e-6    1.76    Toracya T300'),
+                  (6, 'f:  230000  0.27    -0.38e-6    1.80    Torayca T700SC'),
+                  (7, 'f:  235000  0.25    -0.5e-6     1.79    pyrofil TR30S'),
+                  (8, 'f:  640000  0.234   -1.47e-6    2.12    K 63712'),
+                  (9, 'f:  790000  0.23    -1.2e-6     2.15    K 63A12'),
+                  (10, 'f:  294000  0.27    -0.60e-6    1.76    T800S'),
+                  (11, 'f:  900000  0.234   -1.47e-6    2.20    K 13C2U'),
+                  (12, 'f:  339000  0.27    -0.73e-6    1.75    M35J'),
+                  (13, 'f:  436000  0.234   -0.9e-6     1.84    M46J'),
+                  (14, 'f:  242000  0.27    -0.6e-6     1.81    PX35UD'),
+                  (15, 'f:  780000  0.27    -1.5e-6     2.17    XN-80'),
+                  (19, 'f:  73000   0.33    5.3e-6      2.60    e-glas'),
+                  (20, 'f:  80000   0.33    5e-6        2.62    advantex E-CR'),
+                  (22, 'f: 270000   0.25    -6.0e-6     1.56    Zylon'),
+                  (23, 'f: 124000   0.3     -2e-6       1.44    aramide49')]
+    for d in directives:
+        numbers, rest = _get_numbers(d)
+        assert len(numbers) == 4
 
 
 def test_good_fibers():  # {{{1
@@ -83,6 +107,8 @@ def test_good_lamina():
              'test 3': Fiber(240000, 0.2, -0.2e-6, 1.76, 'test 3')}
     r = Resin(3000, 0.3, 20e-6, 1.2, 'resin')
     lamina = []
-    for num, ln in directives:
-        lamina.append(_l(num, ln, fdict, r, 0.5))
+    for d in directives:
+        newlamina = _get_lamina(d, fdict, r, 0.5)
+        if newlamina:
+            lamina.append(newlamina)
     assert len(lamina) == 4
