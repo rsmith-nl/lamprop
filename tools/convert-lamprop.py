131e8aadd9ac41d9e66ac4dec42abcf85bd92352
Roland Smith <rsmith@xs4all.nl>
2018-04-17 21:29:13 +0200

    MIT license for convert-lamprop.

--- tools/convert-lamprop.py
+++ tools/convert-lamprop.py
@@ -3,7 +3,7 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2017-04-16 10:38:30 +0200
-# Last modified: 2017-04-16 15:16:41 +0200
+# Last modified: 2018-04-17T21:28:34+0200
 
 """Script to convert lamprop files from the old style fiber parameters
 (E1, E2, v12, G12, cte1, cte2, density) to the new style parameters
@@ -14,9 +14,25 @@ import logging
 import sys
 import shutil
 
-_lic = '''To the extent possible under law, Roland Smith has waived all copyright and
-related or neighboring rights to convert-lamprop.py. This work is published from the
-Netherlands. See http://creativecommons.org/publicdomain/zero/1.0/'''
+_lic = '''Copyright © 2017 R.F. Smith.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.'''
 
 
 class LicenseAction(argparse.Action):
