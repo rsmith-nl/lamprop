5b42d2c792a173ade1f98353776767fa2a6a55d7
Roland Smith <rsmith@xs4all.nl>
2020-10-25 19:20:31 +0100

    Use a main function in setup.py.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 #
 # Author: R.F. Smith <rsmith@xs4all.nl>
 # Created: 2016-04-24 17:06:48 +0200
-# Last modified: 2020-10-25T17:21:51+0100
+# Last modified: 2020-10-25T19:18:14+0100
 """Install script for lamprop."""
 
 import os
@@ -16,56 +16,11 @@ import tempfile
 import zipfile as z
 
 
-def mkarchive(name, modules, main="__main__.py"):
-    """
-    Create a runnable archive.
-
-    Arguments:
-        name: Name of the archive.
-        modules: Module name or iterable of module names to include.
-        main: Name of the main file. Defaults to __main__.py
-    """
-    # print(f"Building {name}", end="... ")
-    std = "__main__.py"
-    shebang = b"#!/usr/bin/env python\n"
-    if isinstance(modules, str):
-        modules = [modules]
-    if main != std:
-        try:
-            os.remove(std)
-        except OSError:
-            pass
-        os.link(main, std)
-    # Forcibly compile __main__.py lest we use an old version!
-    # Use the same optimization level as for the PyZipFile!
-    py_compile.compile(std, optimize=2)
-    tmpf = tempfile.TemporaryFile()
-    with z.PyZipFile(tmpf, mode="w", compression=z.ZIP_DEFLATED, optimize=2) as zf:
-        zf.writepy(std)
-        for m in modules:
-            zf.writepy(m)
-    if main != std:
-        os.remove(std)
-    tmpf.seek(0)
-    archive_data = tmpf.read()
-    tmpf.close()
-    with open(name, "wb") as archive:
-        archive.write(shebang)
-        archive.write(archive_data)
-    os.chmod(name, 0o755)
-    # print("done.")
-
-
-def remove(path):
-    try:
-        os.remove(path)
-    except FileNotFoundError:
-        pass
-
-
-if __name__ == "__main__":
+def main():
+    """Entry point for the setup script."""
     nm1 = "lamprop"
     nm2 = "lamprop-gui"
+    # Building the script archives.
     scripts = [nm1, nm2]
     if os.name == "nt":
         nm1 += ".pyz"
@@ -74,7 +29,7 @@ if __name__ == "__main__":
     remove(nm2)
     mkarchive(nm1, "lp", main="console.py")
     mkarchive(nm2, "lp", main="gui.py")
-    # Installation
+    # Preparation for installation.
     scheme = os.name + "_user"
     destdir = sysconfig.get_path("scripts", scheme)
     extensions = (".pyw", ".pyz", "")  # Don't change the order!
@@ -104,3 +59,50 @@ if __name__ == "__main__":
                     print(f"* '{src}' would be installed as '{destname}'")
                 # Only the first extension found will be installed.
                 continue
+
+
+def mkarchive(name, modules, main="__main__.py"):
+    """
+    Create a runnable archive.
+
+    Arguments:
+        name: Name of the archive.
+        modules: Module name or iterable of module names to include.
+        main: Name of the main file. Defaults to __main__.py
+    """
+    std = "__main__.py"
+    shebang = b"#!/usr/bin/env python\n"
+    if isinstance(modules, str):
+        modules = [modules]
+    if main != std:
+        remove(std)
+        os.link(main, std)
+    # Optimization level for compile.
+    lvl = 2
+    # Forcibly compile __main__.py lest we use an old version!
+    py_compile.compile(std, optimize=lvl)
+    with tempfile.TemporaryFile() as tmpf:
+        with z.PyZipFile(tmpf, mode="w", compression=z.ZIP_DEFLATED, optimize=lvl) as zf:
+            zf.writepy(std)
+            for m in modules:
+                zf.writepy(m)
+        if main != std:
+            remove(std)
+        tmpf.seek(0)
+        archive_data = tmpf.read()
+    with open(name, "wb") as archive:
+        archive.write(shebang)
+        archive.write(archive_data)
+    os.chmod(name, 0o755)
+
+
+def remove(path):
+    """Remove a file, ignoring directories and nonexistant files."""
+    try:
+        os.remove(path)
+    except (FileNotFoundError, PermissionError, IsADirectoryError):
+        pass
+
+
+if __name__ == "__main__":
+    main()
