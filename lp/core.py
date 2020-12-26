d937354d71e0759fe3e95fc7b2f536113bca4335
Roland Smith <rsmith@xs4all.nl>
2020-12-26 22:05:12 +0100

    Added function to convert stiffness to Abaqus format.

--- lp/core.py
+++ lp/core.py
@@ -4,7 +4,7 @@
 # Copyright © 2014-2020 R.F. Smith <rsmith@xs4all.nl>. All rights reserved.
 # SPDX-License-Identifier: BSD-2-Clause
 # Created: 2014-02-21 22:20:39 +0100
-# Last modified: 2020-12-26T18:11:34+0100
+# Last modified: 2020-12-26T22:04:23+0100
 """
 Core functions of lamprop.
 
@@ -82,7 +82,6 @@ The following references were used in coding this module:
   number =       {Reference Publication 1351}
 }
 """
-
 from types import SimpleNamespace
 import math
 import lp.matrix as lpm
@@ -515,3 +514,17 @@ def isti(C):
     ):
         return True
     return False
+
+
+def toabaqusi(C):
+    """Convert stiffness matrix to Abaqus format and SI units."""
+    D = lpm.mul(C, 1e6)
+    D[0][3] = C[0][5]
+    D[0][5] = C[0][3]
+    D[1][3] = C[1][5]
+    D[1][5] = C[1][3]
+    D[2][3] = C[2][5]
+    D[2][5] = C[2][3]
+    D[3][3] = C[5][5]
+    D[5][5] = C[3][3]
+    return D
