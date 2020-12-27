a29d0ca35e5901c8b50dfdc25018accf6af397e7
Roland Smith <rsmith@xs4all.nl>
2020-12-27 23:43:21 +0100

    Updated LaTeX ouptut.

--- lp/latex.py
+++ lp/latex.py
@@ -4,13 +4,14 @@
 # Copyright © 2011-2020 R.F. Smith <rsmith@xs4all.nl>. All rights reserved.
 # SPDX-License-Identifier: BSD-2-Clause
 # Created: 2011-03-27 23:19:38 +0200
-# Last modified: 2020-12-27T01:16:21+0100
+# Last modified: 2020-12-27T23:25:14+0100
 """LaTeX output routines for lamprop."""
 
 from .version import __version__
+from lp.text import _fea as _fea_text
 
 
-def out(lam, eng, mat, fea):
+def out(lam, eng, mat, fea):  # {{{1
     """Output function for LaTeX format. Returns a list of lines."""
     texlname = lam.name.replace('_', r'\_')
     lines = [
@@ -20,7 +21,7 @@ def out(lam, eng, mat, fea):
         "  \\centering\\footnotesize{\\rule{0pt}{10pt}",
         f"  \\tiny calculated by lamprop {__version__}\\\\[3pt]}}",
         "    \\begin{tabular}[t]{rcrrl}",
-        "      \\multicolumn{4}{c}{\\small\\textbf{Laminate stacking}}\\\\[0.1em]",
+        "      \\multicolumn{5}{c}{\\small\\textbf{Laminate stacking}}\\\\[0.1em]",
         "      \\toprule %% \\usepackage{booktabs}",
         "      Layer & Weight & Angle & vf & Fiber type\\\\",
         "            & [g/m$^2$] & [$\\circ$] & [\\%]\\\\",
@@ -38,17 +39,19 @@ def out(lam, eng, mat, fea):
         lines += _engprop(lam)
     if mat:
         lines += _matrices(lam)
+    if fea:
+        lines += _fea(lam)
     lines.append("\\end{table}")
     lines.append("")
     return lines
 
 
-def _engprop(l):
+def _engprop(l):  # {{{1
     """Return the engineering properties as a LaTeX table in the form of
     a list of lines."""
     w = l.fiber_weight + l.resin_weight
     lines = [
-        "    \\begin{tabular}[t]{rrl}",
+        "    \\begin{tabular}[t]{rrlrrl}",
         "      \\multicolumn{3}{c}{\\small\\textbf{Engineering properties}}\\\\[0.1em]",
         "      \\toprule",
         "      Property & Value & Dimension\\\\",
@@ -56,40 +59,36 @@ def _engprop(l):
         f"      $\\mathrm{{v_f}}$ & {l.vf*100:.3g} &\\%\\\\",
         f"      $\\mathrm{{w_f}}$ & {l.wf*100:.3g} &\\%\\\\",
         f"      thickness & {l.thickness:.3g} & mm\\\\",
-        "      density & {l.ρ:.3g} & g/cm$^3$\\\\",
+        f"      density & {l.ρ:.3g} & g/cm$^3$\\\\",
         f"      weight & {w:.0f} & g/m$^2$\\\\",
-        "      resin & {l.resin_weight:.0f} & g/m$^2$\\\\",
+        f"      resin & {l.resin_weight:.0f} & g/m$^2$\\\\",
         "      \\midrule",
-        "      \\multicolumn{3}{c}{\\small\\textbf{In-plane}}\\\\[0.1em]",
-        f"      $\\mathrm{{E_x}}$ & {l.Ex:8.0f} & MPa\\\\",
-        f"      $\\mathrm{{E_y}}$ & {l.Ey:8.0f} & MPa\\\\",
-        f"      $\\mathrm{{E_z}}$ & {l.Ez:8.0f} & MPa\\\\",
-        f"      $\\mathrm{{G_{{xy}}}}$ & {l.Gxy:8.0f} & MPa\\\\",
-        f"      $\\mathrm{{G_{{xz}}}}$ & {l.Gxz:8.0f} & MPa\\\\",
-        f"      $\\mathrm{{G_{{yz}}}}$ & {l.Gyz:8.0f} & MPa\\\\",
-        f"      $\\mathrm{{\\nu_{{xy}}}}$ & {l.νxy:g} &-\\\\",
-        f"      $\\mathrm{{\\nu_{{yx}}}}$ & {l.νyx:g} &-\\\\",
-        f"      $\\mathrm{{\\alpha_x}}$ & {l.αx:g} & K$^{{-1}}$\\\\",
+        "      \\multicolumn{3}{c}{\\small\\textbf{In-plane}} & ",
+        "\\multicolumn{3}{c}{\\small\\textbf{3D stiffness tensor}}\\\\[0.1em]",
+        f"      $\\mathrm{{E_x}}$ & {l.Ex:8.0f} & MPa & $\\mathrm{{E_x}}$ & {l.tEx:8.0f} & MPa\\\\",
+        f"      $\\mathrm{{E_y}}$ & {l.Ey:8.0f} & MPa & $\\mathrm{{E_y}}$ & {l.tEy:8.0f} & MPa\\\\",
+        f"      $\\mathrm{{E_z}}$ & {l.Ez:8.0f} & MPa & $\\mathrm{{E_z}}$ & {l.tEz:8.0f} & MPa\\\\",
+        f"      $\\mathrm{{G_{{xy}}}}$ & {l.Gxy:8.0f} & MPa &"
+        f" $\\mathrm{{G_{{xy}}}}$ & {l.tGxy:8.0f} & MPa\\\\",
+        f"      $\\mathrm{{G_{{xz}}}}$ & {l.Gxz:8.0f} & MPa &"
+        f" $\\mathrm{{G_{{xz}}}}$ & {l.tGxz:8.0f} & MPa\\\\",
+        f"      $\\mathrm{{G_{{yz}}}}$ & {l.Gyz:8.0f} & MPa & "
+        f" $\\mathrm{{G_{{yz}}}}$ & {l.tGyz:8.0f} & MPa\\\\",
+        f"      $\\mathrm{{\\nu_{{xy}}}}$ & {l.νxy:g} &- &"
+        f"$\\mathrm{{\\nu_{{xy}}}}$ & {l.tνxy:g} &-\\\\",
+        f"      $\\mathrm{{\\nu_{{yx}}}}$ & {l.νyx:g} &- & "
+        f"$\\mathrm{{\\nu_{{xz}}}}$ & {l.tνxz:g} &-\\\\",
+        f"      $\\mathrm{{\\alpha_x}}$ & {l.αx:g} & K$^{{-1}}$ &"
+        f"$\\mathrm{{\\nu_{{yz}}}}$ & {l.tνyz:g} &-\\\\",
         f"      $\\mathrm{{\\alpha_y}}$ & {l.αy:g} & K$^{{-1}}$\\\\",
-        "      \\midrule",
-        "      \\multicolumn{3}{c}{\\small\\textbf{From 3D stiffness matrix}}\\\\[0.1em]",
-        f"      $\\mathrm{{E_x}}$ & {l.tEx:8.0f} & MPa\\\\",
-        f"      $\\mathrm{{E_y}}$ & {l.tEy:8.0f} & MPa\\\\",
-        f"      $\\mathrm{{E_z}}$ & {l.tEz:8.0f} & MPa\\\\",
-        f"      $\\mathrm{{G_{{xy}}}}$ & {l.Gxy:8.0f} & MPa\\\\",
-        f"      $\\mathrm{{G_{{xz}}}}$ & {l.Gxz:8.0f} & MPa\\\\",
-        f"      $\\mathrm{{G_{{yz}}}}$ & {l.Gyz:8.0f} & MPa\\\\",
-        f"      $\\mathrm{{\\nu_{{xy}}}}$ & {l.tνxy:g} &-\\\\",
-        f"      $\\mathrm{{\\nu_{{xz}}}}$ & {l.tνxz:g} &-\\\\",
-        f"      $\\mathrm{{\\nu_{{yz}}}}$ & {l.tνyz:g} &-\\\\",
         "      \\bottomrule",
         "    \\end{tabular}",
     ]
     return lines
 
 
-def _matrices(l):
-    """Return the ABD and abd matrices as LaTeX arrays in the form of
+def _matrices(l):  # {{{1
+    """Return the matrices as LaTeX arrays in the form of
     a list of lines."""
     def pm(mat, r=6):
         """Return the contents of a matrix."""
@@ -110,7 +109,7 @@ def _matrices(l):
         return lines
     lines = [
         "  \\vbox{",
-        "    \\vbox{\\small\\textbf{Stiffness (ABD) matrix}\\\\[-5mm]",
+        "    \\vbox{\\small\\textbf{In-plane stiffness (ABD) matrix}\\\\[-3mm]",
         "      \\tiny\\[\\left\\{\\begin{array}{c}",
         "          N_x\\\\ N_y\\\\ N_{xy}\\\\ M_x\\\\ M_y\\\\ M_{xy}",
         "        \\end{array}\\right\\} = ",
@@ -120,8 +119,8 @@ def _matrices(l):
     lines += [
         "          \\end{array}\\right| \\times",
         "        \\left\\{\\begin{array}{c}",
-        "            \\epsilon_x\\\\[3pt] \\epsilon_y\\\\[3pt] \\gamma_{xy}\\\\[3pt]",
-        "            \\kappa_x\\\\[3pt] \\kappa_y\\\\[3pt] \\kappa_{xy}",
+        "            \\epsilon_x\\\\[2pt] \\epsilon_y\\\\[2pt] \\gamma_{xy}\\\\[2pt]",
+        "            \\kappa_x\\\\[2pt] \\kappa_y\\\\[2pt] \\kappa_{xy}",
         "          \\end{array}\\right\\}\\]",
         "    }",
         "    \\vbox{\\small\\textbf{Transverse stiffness (H) matrix}\\\\[-2mm]",
@@ -134,35 +133,40 @@ def _matrices(l):
     lines += [
         "          \\end{array}\\right| \\times",
         "        \\left\\{\\begin{array}{c}",
-        "            \\gamma_{yz}\\\\[3pt] \\gamma_{xz}",
+        "            \\gamma_{yz}\\\\[2pt] \\gamma_{xz}",
         "          \\end{array}\\right\\}\\]",
         "    }",
-        "    \\vbox{\\small\\textbf{Compliance (abd) matrix}\\\\[-5mm]",
-        "      \\tiny\\[\\left\\{\\begin{array}{c}",
-        "            \\epsilon_x\\\\[3pt] \\epsilon_y\\\\[3pt] \\gamma_{xy}\\\\[3pt]",
-        "            \\kappa_x\\\\[3pt] \\kappa_y\\\\[3pt] \\kappa_{xy}",
-        "          \\end{array}\\right\\} = \\left|\\begin{array}{cccccc}",
     ]
-    lines += pm(l.abd)
     lines += [
-        "          \\end{array}\\right|\\times",
-        "        \\left\\{\\begin{array}{c}",
-        "            N_x\\\\ N_y\\\\ N_{xy}\\\\ M_x\\\\ M_y\\\\ M_{xy}",
-        "          \\end{array}\\right\\}\\]\\\\",
-        "    }",
-        "    \\vbox{\\small\\textbf{Transverse compliance (h) matrix}\\\\[-2mm]",
+        "    \\vbox{\\small\\textbf{3D Stiffness matrix (C), contracted notation}\\\\[-3mm]",
         "      \\tiny\\[\\left\\{\\begin{array}{c}",
-        "          \\gamma_{yz}\\\\[3pt] \\gamma_{xz}",
+        "          \\sigma_{11}\\\\ \\sigma_{22}\\\\ \\sigma_{33}\\\\ \\sigma_{23}\\\\ \\sigma_{13}\\\\ \\sigma_{12}",
         "        \\end{array}\\right\\} = ",
-        "      \\left|\\begin{array}{cc}"
+        "      \\left|\\begin{array}{cccccc}",
     ]
-    lines += pm(l.h, r=2)
+    lines += pm(l.C)
     lines += [
         "          \\end{array}\\right| \\times",
         "        \\left\\{\\begin{array}{c}",
-        "            V_y\\\\ V_x",
+        "            \\epsilon_{11}\\\\[2pt] \\epsilon_{22}\\\\[2pt] \\epsilon_{33}\\\\[2pt]",
+        "            2\\cdot\\epsilon_{23}\\\\[2pt] 2\\cdot\\epsilon_{13}\\\\[2pt] 2\\cdot\\epsilon_{12}",
         "          \\end{array}\\right\\}\\]",
         "    }",
+        "  }",
+    ]
+    return lines
+
+
+def _fea(l):  # {{{1
+    lines = [
+        "  \\vbox{",
+        "  \\begin{centering}",
+        "  \\begin{verbatim}",
+    ]
+    lines += _fea_text(l)
+    lines += [
+        "  \\end{verbatim}",
+        "  \\end{centering}",
         "  }",
     ]
     return lines
