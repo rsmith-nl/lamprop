ad126c79f230eb41a3772aa6e62662e91ed35da4
Roland Smith <rsmith@xs4all.nl>
2020-12-27 01:15:52 +0100

    Updated LaTeX output.

--- lp/latex.py
+++ lp/latex.py
@@ -1,25 +1,38 @@
 # file: latex.py
 # vim:fileencoding=utf-8:ft=python:fdm=marker
 #
-# Copyright © 2011-2019 R.F. Smith <rsmith@xs4all.nl>. All rights reserved.
+# Copyright © 2011-2020 R.F. Smith <rsmith@xs4all.nl>. All rights reserved.
 # SPDX-License-Identifier: BSD-2-Clause
 # Created: 2011-03-27 23:19:38 +0200
-# Last modified: 2019-05-05T19:43:07+0200
+# Last modified: 2020-12-27T01:16:21+0100
 """LaTeX output routines for lamprop."""
 
 from .version import __version__
 
 
-def out(lam, eng, mat):
+def out(lam, eng, mat, fea):
     """Output function for LaTeX format. Returns a list of lines."""
     texlname = lam.name.replace('_', r'\_')
-    txt = "  \\caption{{\\label{{tab:{0}}}properties of {0}}}"
     lines = [
         "\\begin{table}[!htbp]",
         "  \\renewcommand{\\arraystretch}{1.2}",
-        txt.format(texlname),
+        f"  \\caption{{\\label{{tab:{texlname}}}properties of {texlname}}}",
         "  \\centering\\footnotesize{\\rule{0pt}{10pt}",
-        "  \\tiny calculated by lamprop {}\\\\[3pt]}}".format(__version__)
+        f"  \\tiny calculated by lamprop {__version__}\\\\[3pt]}}",
+        "    \\begin{tabular}[t]{rcrrl}",
+        "      \\multicolumn{4}{c}{\\small\\textbf{Laminate stacking}}\\\\[0.1em]",
+        "      \\toprule %% \\usepackage{booktabs}",
+        "      Layer & Weight & Angle & vf & Fiber type\\\\",
+        "            & [g/m$^2$] & [$\\circ$] & [\\%]\\\\",
+        "      \\midrule",
+    ]
+    for ln, la in enumerate(lam.layers, start=1):
+        s = "      {} & {:4.0f} & {:5.0f} & {:.3g} & {}\\\\"
+        texfname = la.fiber.name.replace('_', r'\_')
+        lines.append(s.format(ln, la.fiber_weight, la.angle, la.vf*100, texfname))
+    lines += [
+        "      \\bottomrule",
+        "    \\end{tabular}\\hspace{0.02\\textwidth}",
     ]
     if eng:
         lines += _engprop(lam)
@@ -33,43 +46,42 @@ def out(lam, eng, mat):
 def _engprop(l):
     """Return the engineering properties as a LaTeX table in the form of
     a list of lines."""
+    w = l.fiber_weight + l.resin_weight
     lines = [
-        "    \\begin{tabular}[t]{rcrrl}",
-        "      \\multicolumn{4}{c}{\\small\\textbf{Laminate stacking}}\\\\[0.1em]",
-        "      \\toprule %% \\usepackage{booktabs}",
-        "      Layer & Weight & Angle & vf & Fiber type\\\\",
-        "            & [g/m$^2$] & [$\\circ$] & [\\%]\\\\",
-        "      \\midrule",
-    ]
-    for ln, la in enumerate(l.layers, start=1):
-        s = "      {} & {:4.0f} & {:5.0f} & {:.3g} & {}\\\\"
-        texfname = la.fiber.name.replace('_', r'\_')
-        lines.append(s.format(ln, la.fiber_weight, la.angle, la.vf*100, texfname))
-    lines += [
-        "      \\bottomrule",
-        "    \\end{tabular}\\hspace{0.02\\textwidth}",
         "    \\begin{tabular}[t]{rrl}",
         "      \\multicolumn{3}{c}{\\small\\textbf{Engineering properties}}\\\\[0.1em]",
         "      \\toprule",
         "      Property & Value & Dimension\\\\",
         "      \\midrule",
-        "      $\\mathrm{{v_f}}$ & {:.3g} &\\%\\\\".format(l.vf*100),
-        "      $\\mathrm{{w_f}}$ & {:.3g} &\\%\\\\".format(l.wf*100),
-        "      thickness & {:.3g} & mm\\\\".format(l.thickness),
-        "      density & {:.3g} & g/cm$^3$\\\\".format(l.ρ),
-        "      weight & {:.0f} & g/m$^2$\\\\".format(l.fiber_weight+l.resin_weight),
-        "      resin & {:.0f} & g/m$^2$\\\\".format(l.resin_weight),
+        f"      $\\mathrm{{v_f}}$ & {l.vf*100:.3g} &\\%\\\\",
+        f"      $\\mathrm{{w_f}}$ & {l.wf*100:.3g} &\\%\\\\",
+        f"      thickness & {l.thickness:.3g} & mm\\\\",
+        "      density & {l.ρ:.3g} & g/cm$^3$\\\\",
+        f"      weight & {w:.0f} & g/m$^2$\\\\",
+        "      resin & {l.resin_weight:.0f} & g/m$^2$\\\\",
         "      \\midrule",
-        "      $\\mathrm{{E_x}}$ & {:8.0f} & MPa\\\\".format(l.Ex),
-        "      $\\mathrm{{E_y}}$ & {:8.0f} & MPa\\\\".format(l.Ey),
-        "      $\\mathrm{{E_z}}$ & {:8.0f} & MPa\\\\".format(l.Ez),
-        "      $\\mathrm{{G_{{xy}}}}$ & {:8.0f} & MPa\\\\".format(l.Gxy),
-        "      $\\mathrm{{G_{{xz}}}}$ & {:8.0f} & MPa\\\\".format(l.Gxz),
-        "      $\\mathrm{{G_{{yz}}}}$ & {:8.0f} & MPa\\\\".format(l.Gyz),
-        "      $\\mathrm{{\\nu_{{xy}}}}$ & {:g} &-\\\\".format(l.νxy),
-        "      $\\mathrm{{\\nu_{{yx}}}}$ & {:g} &-\\\\".format(l.νyx),
-        "      $\\mathrm{{\\alpha_x}}$ & {:g} & K$^{{-1}}$\\\\".format(l.αx),
-        "      $\\mathrm{{\\alpha_y}}$ & {:g} & K$^{{-1}}$\\\\".format(l.αy),
+        "      \\multicolumn{3}{c}{\\small\\textbf{In-plane}}\\\\[0.1em]",
+        f"      $\\mathrm{{E_x}}$ & {l.Ex:8.0f} & MPa\\\\",
+        f"      $\\mathrm{{E_y}}$ & {l.Ey:8.0f} & MPa\\\\",
+        f"      $\\mathrm{{E_z}}$ & {l.Ez:8.0f} & MPa\\\\",
+        f"      $\\mathrm{{G_{{xy}}}}$ & {l.Gxy:8.0f} & MPa\\\\",
+        f"      $\\mathrm{{G_{{xz}}}}$ & {l.Gxz:8.0f} & MPa\\\\",
+        f"      $\\mathrm{{G_{{yz}}}}$ & {l.Gyz:8.0f} & MPa\\\\",
+        f"      $\\mathrm{{\\nu_{{xy}}}}$ & {l.νxy:g} &-\\\\",
+        f"      $\\mathrm{{\\nu_{{yx}}}}$ & {l.νyx:g} &-\\\\",
+        f"      $\\mathrm{{\\alpha_x}}$ & {l.αx:g} & K$^{{-1}}$\\\\",
+        f"      $\\mathrm{{\\alpha_y}}$ & {l.αy:g} & K$^{{-1}}$\\\\",
+        "      \\midrule",
+        "      \\multicolumn{3}{c}{\\small\\textbf{From 3D stiffness matrix}}\\\\[0.1em]",
+        f"      $\\mathrm{{E_x}}$ & {l.tEx:8.0f} & MPa\\\\",
+        f"      $\\mathrm{{E_y}}$ & {l.tEy:8.0f} & MPa\\\\",
+        f"      $\\mathrm{{E_z}}$ & {l.tEz:8.0f} & MPa\\\\",
+        f"      $\\mathrm{{G_{{xy}}}}$ & {l.Gxy:8.0f} & MPa\\\\",
+        f"      $\\mathrm{{G_{{xz}}}}$ & {l.Gxz:8.0f} & MPa\\\\",
+        f"      $\\mathrm{{G_{{yz}}}}$ & {l.Gyz:8.0f} & MPa\\\\",
+        f"      $\\mathrm{{\\nu_{{xy}}}}$ & {l.tνxy:g} &-\\\\",
+        f"      $\\mathrm{{\\nu_{{xz}}}}$ & {l.tνxz:g} &-\\\\",
+        f"      $\\mathrm{{\\nu_{{yz}}}}$ & {l.tνyz:g} &-\\\\",
         "      \\bottomrule",
         "    \\end{tabular}",
     ]
